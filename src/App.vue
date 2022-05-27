4347b10b779e0cbda0c080c64a55bf58be12cdb9
radiorz <s565338107@163.com>
2022-05-28 00:22:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="font-chinese antialiased">
+    <Navbar />
+    <RouterView />
+    <Footer />
+  </div>
+</template>
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import { logLabeled } from "./utils/logger";
+logLabeled("I'm ready!  ⸜(๑'ᵕ'๑)⸝⋆*", "log", "", "color: #66ccff");
+</script>
